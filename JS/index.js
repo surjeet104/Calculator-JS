ae572119a537b05b6d364ab5a2d4ba50bf6bb0ae
surjeet104 <singh.surjeet67@gmail.com>
2023-06-03 13:06:45 +0530

    first commit

---
+++ JS/index.js
@@ -0,0 +1,38 @@
+var display = $("#display");
+//var buttons=Array.from($(".button"));
+// for(var i=0;i<buttons.length;i++){
+    // console.log(buttons[i]);
+    // $(".button").on("click",function(){
+    //     console.log("clicked");
+    //     console.log($(this).html());
+    // });
+// }
+
+
+var string = "";
+$(".button").on("click", (e) => {
+    switch (e.target.innerText) {
+        case "C":
+            string="";
+            display.text(string);
+            break;
+        case "←":
+            if(string){
+                string = string.slice(0, -1);
+                display.text(string);
+            }
+            break;
+        case "=":
+            try {
+                string = eval(string);
+                display.text(string);
+            } catch (error) {
+                string="";
+                display.text("Error!");
+            }
+            break;
+        default:
+            string += e.target.innerText;
+            display.text(string);
+    }
+});
